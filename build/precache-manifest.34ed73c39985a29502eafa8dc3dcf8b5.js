88b766caf995946237da4d30b2ee5f4802c2b80c
Daniel Andres Gallo Garcia <dgallo@teravisiontech.com>
2020-06-25 22:53:31 -0500

    [project-torre] feat(build)

---
+++ build/precache-manifest.34ed73c39985a29502eafa8dc3dcf8b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88785f7f5fec13c4bba06916ee139cb",
+    "url": "/project-torre/index.html"
+  },
+  {
+    "revision": "7109158fb5b72fe90d8e",
+    "url": "/project-torre/static/js/2.f38d6538.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-torre/static/js/2.f38d6538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092d42362f371fee2ac4",
+    "url": "/project-torre/static/js/main.1da91f5d.chunk.js"
+  },
+  {
+    "revision": "a42ce0a2af7d90995dad",
+    "url": "/project-torre/static/js/runtime-main.53d9e13d.js"
+  }
+]);
